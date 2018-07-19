454af4d4c58500ce1e0112e6492e65db41213b03
Cristiano Gorla <cristiano.gorla@gruppobdf.com>
2018-07-19 18:12:51 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,35 @@ import { BookComponent } from './book/book.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { BookCreateComponent } from './book-create/book-create.component';
 import { BookEditComponent } from './book-edit/book-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+    {
+        path: 'books',
+        component: BookComponent,
+        data: { title: 'Book List' }
+    },
+    {
+        path: 'book-details/:id',
+        component: BookDetailComponent,
+        data: { title: 'Book Details' }
+    },
+    {
+        path: 'book-create',
+        component: BookCreateComponent,
+        data: { title: 'Create Book' }
+    },
+    {
+        path: 'book-edit/:id',
+        component: BookEditComponent,
+        data: { title: 'Edit Book' }
+    },
+    { path: '',
+        redirectTo: '/books',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +45,9 @@ import { BookEditComponent } from './book-edit/book-edit.component';
     BookEditComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
