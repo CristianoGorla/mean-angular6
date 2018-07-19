59dd591f18eb4daab3238714a6b57f0b4f19cb37
CristianoGorlaBDF <cristiano.gorla@gruppobdf.com>
2018-07-19 15:47:49 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
